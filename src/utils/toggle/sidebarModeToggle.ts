2dd1dc73da39352587350dcd9e3061246d418a16
curious_andrew <afirsov410@gmail.com>
2025-01-17 10:58:31 +0300

    sidebar default mode

--- src/utils/toggle/sidebarModeToggle.ts
+++ src/utils/toggle/sidebarModeToggle.ts
@@ -2,6 +2,6 @@ import { Toggle } from "@/utils/toggle/Toggle.class";
 import { SidebarMode } from "@/utils/consts";
 
 export const sidebarModeToggle = new Toggle([
-  SidebarMode.ShowAllLevels,
   SidebarMode.ShowCurrentLevel,
+  SidebarMode.ShowAllLevels,
 ]);
